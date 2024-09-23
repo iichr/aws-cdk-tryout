41223ebd99e1b067aae53e40e5338dca3ae0034e
Chris I <chris-ii@hotmail.co.uk>
2024-09-23 21:34:12 +0100

    feat: add hit counter dynamodb lambda

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -4,6 +4,7 @@ import { Code, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hit-counter';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -16,8 +17,12 @@ export class CdkWorkshopStack extends Stack {
       bundling: { minify: true },
     })
 
+    const hitCounter = new HitCounter(this, "HitCounter", {
+      targetFunction: testLambda,
+    })
+
     const gateway = new LambdaRestApi(this, "Endpoint", {
-      handler: testLambda,
+      handler: hitCounter.handler,
     })
   }
 }
---
+++ lib/hit-counter.ts
@@ -0,0 +1,42 @@
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { IFunction, Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { table } from "console";
+import { Construct } from "constructs";
+import path = require("path");
+
+export interface HitCounterProps {
+    targetFunction: IFunction;
+}
+
+export class HitCounter extends Construct {
+    public readonly handler: NodejsFunction
+
+    constructor(scope: Construct, id: string, props: HitCounterProps) {
+        super(scope, id)
+
+        const hitsTable = new Table(this, "Hits", {
+            partitionKey: { name: "path", type: AttributeType.STRING }
+        })
+
+        this.handler = new NodejsFunction(this, "HitCounterHandler", {
+            entry: path.join(__dirname, '../lambda/hit-counter.ts'),
+            handler: 'handler',
+            runtime: Runtime.NODEJS_20_X,
+            bundling: { minify: true },
+            environment: {
+                HITS_TABLE_NAME: hitsTable.tableName,
+                TARGET_FUNCTION_NAME: props.targetFunction.functionName,
+            }
+        }
+        )
+
+        // Grant lambda permissions to write data to the table
+        hitsTable.grantReadWriteData(this.handler)
+
+        // Grant lambda permissions to invoke the target function
+        props.targetFunction.grantInvoke(this.handler)
+    }
+
+
+}
